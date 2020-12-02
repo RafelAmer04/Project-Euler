df035be1071ec37af4571f68bb562812d9946bf4
Rafel Amer <rafel.amer@upc.edu>
2020-12-02 09:13:14 +0100

    Corregit el problema 4

--- P004.py
+++ P004.py
@@ -1,9 +1,11 @@
+m = 0
 
-for a in range(100, 999):
-    for b in range(100, 999):
-        string = str(a * b)
+for a in range(100, 1000):
+    for b in range(100, 1000):
+        n = a * b
+        string = str(n)
         rev = string[::-1]
-        if string == rev:
-            sol = string
+        if string == rev and n > m:
+            m = n
 
-print(sol)
+print(m)
