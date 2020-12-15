33313f3b55be864160c328e86fb50b2d27020b0f
Rafel Amer <rafel.amer@upc.edu>
2020-12-15 19:24:26 +0100

    Co0rreccions al fitxer P01.py

--- P019.py
+++ P019.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8
+
 def IsLeap(y):
     if y % 400 == 0:
         return True
@@ -25,33 +28,72 @@ def NDaysMonth(m, y):
 
 
 def CountDaysFrom(f1, f2):
+    if f1[0] == f2[0] and f1[1] == f2[1] and f1[2] == f2[2]:
+        return 0
     t = 0
-    for i in range(f1[2]+1, f2[2]):
-        t += NDaysYear(i)
-    t += NDaysMonth(f1[1], f1[2]) - f1[0]
-    for i in range(f1[1]+1, 13):
-        t += NDaysMonth(i, f1[2])
+    #
+    # Anys sencers entre les dues dates
+    #
+    for y in range(f1[2]+1, f2[2]):
+        t += NDaysYear(y)
+    #
+    # No esten el matreix any
+    #
+    if f2[2] > f1[2]:
+        #
+        # Mesos sencers des de la primera data fins a finals d'any
+        #
+        for m in range (f1[1] + 1,13):
+            t += NDaysMonth(m,f1[2])
+        #
+        # Mesos sencers des de principis d'any fins a la segona data
+        #
+        for m in range (1,f2[1]):
+            t += NDaysMonth(m,f2[2])
+        #
+        # Dies de l'últim mes
+        #
+        t += f2[0]
+        #
+        # Dies del primer mes
+        #
+        t += NDaysMonth(f1[1],f1[2]) - f1[0]
+        return t
+    #
+    # Estem al mateix any
+    #     Mesos sencers entre les dues dates
+    #
+    for m in range(f1[1] + 1,f2[1]):
+        t += NDaysMonth(m,f1[2])
+    #
+    # Dies de l'últim mes
+    #
     t += f2[0]
-    for i in range(1, f2[1]):
-        t += NDaysMonth(i, f2[2])
+    #
+    # Dies del primer mes
+    #
+    t += NDaysMonth(f1[1],f1[2]) - f1[0]
     return t
 
+# d1 = (1,1,1900)
+# sol = 0
+#
+#
+# for y in range(1901, 7000):
+#     for m in range(1, 13):
+#         if CountDaysFrom(d1, (1,m,y)) % 7 == 6:
+#             sol += 1
 
-d1 = (1,1,1900)
+
+
 sol = 0
-
-for y in range(1901, 2001):
+d = 0
+for y in range(1901,7000):
+    d += CountDaysFrom((1,1,y-1),(1,1,y))
+    d = d % 7
     for m in range(1, 13):
-        if CountDaysFrom(d1, (1,m,y)) % 7 == 6:
+        s = CountDaysFrom((1,1,y), (1,m,y))
+        if (1 + d + s) % 7 == 0:
             sol += 1
 
-
-
-
-print(sol)
-
-
-
-
-
-
+print (sol)
