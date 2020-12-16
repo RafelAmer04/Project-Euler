65ffe752a7475c88d4d7902386a0ac3332e4f37a
RafelAmer04 <legorafel@gmail.com>
2020-12-16 15:33:58 +0100

    P022
    Solved P022

--- P022.py
+++ P022.py
@@ -1,6 +1,49 @@
+def score(p, n):
+    s = 0
+    d = {
+        "A": 1,
+        "B": 2,
+        "C": 3,
+        "D": 4,
+        "E": 5,
+        "F": 6,
+        "G": 7,
+        "H": 8,
+        "I": 9,
+        "J": 10,
+        "K": 11,
+        "L": 12,
+        "M": 13,
+        "N": 14,
+        "O": 15,
+        "P": 16,
+        "Q": 17,
+        "R": 18,
+        "S": 19,
+        "T": 20,
+        "U": 21,
+        "V": 22,
+        "W": 23,
+        "X": 24,
+        "Y": 25,
+        "Z": 26,
+    }
+    for i in range(len(p)):
+        l = p[i]
+        s += d[l]
+    s *= n
+    return s
+
+
 f = open("P022.txt", "r")
 t = f.read()
-t = t.replace('"', " ")
+l = t.replace(']','').replace('[','')
+l = l.replace('"','').split(",")
 
+s = sorted(l)
 
-print(t)
+sol = 0
+
+for i in range(len(s)):
+    sol += score(s[i], i+1)
+print(sol)
--- P023.py
+++ P023.py
@@ -17,4 +17,4 @@ for i in range(12, 28124):
 
 for i in range(1, 28123):
     for j in range(len(a)):
-        if i
+
