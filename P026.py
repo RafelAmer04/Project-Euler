0a41eca1ca0d9739bdf75461725920dda8cf6866
Rafel Amer <rafel.amer@upc.edu>
2021-01-14 20:03:50 +0100

    Problema 26

--- P026.py
+++ P026.py
@@ -1,4 +1,27 @@
-from decimal import *
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
 
-getcontext().prec = 1000
-print(Decimal(1)/Decimal(243))
+def length_recurring_cycle(n):
+	r = 1
+	remainders = []
+	while True:
+		r = r % n
+		if r == 0:
+			return 0
+		if r in remainders:
+			i = remainders.index(r)
+			return len(remainders) - i
+		else:
+			remainders.append(r)
+		r *= 10
+
+max = 0
+value = 0
+for i in xrange(1,1000):
+	l = length_recurring_cycle(i)
+	if l > max:
+		max = l
+		value = i
+
+print "The value of d < 1000 for which 1/d contains the longest recurring cycle is: %d" % value
