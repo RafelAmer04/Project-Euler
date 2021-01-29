ee79ffc2a864ffe73c54c33555c57b6c94e30081
Rafel Amer <legorafel@gmail.com>
2021-01-29 19:48:01 +0100

    Solved P041

--- P041.py
+++ P041.py
@@ -4,18 +4,17 @@ from itertools import permutations
 
 t = "123456789"
 perm = [''.join(p) for p in permutations(t)]
-
-
-
 lar = 0
 
-for n in range(10):
-    for i in perm:
-        if IsPrime(i) and int(i) > lar:
-            lar = int(i)
+
+
+while len(t) > 1:
+    for p in permutations(t):
+        s = "".join(p)
+        if IsPrime(s) and int(s) > lar:
+            lar = int(s)
     if lar != 0:
         break
     t = t[:-1]
-    perm = [''.join(p) for p in permutations(t)]
 
 print(lar)
