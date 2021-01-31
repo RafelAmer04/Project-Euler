772763fd9240fc7efdd5100bd0d89915272ac482
Rafel Amer <legorafel@gmail.com>
2021-01-31 20:12:49 +0100

    Created /Python

--- Python/P044.py
+++ Python/P044.py
@@ -42,3 +42,27 @@ while D == 0:
     n += 1
 
 print(D)
+
+
+# import sys
+#
+# def is_pentagonal(x):
+#     t = (1.0+math.sqrt(1.0+24.0*x))/6.0
+#     if t.is_integer():
+#         return True
+#     return False
+#
+# def pentagonal(n):
+#     return n*(3*n-1)/2
+#
+# n = 1
+# while True:
+#     p = pentagonal(n)
+#     m = 1
+#     while m < n:
+#         q = pentagonal(m)
+#         if is_pentagonal(p-q) and is_pentagonal(p+q):
+#             print(p, q, p-q, p+q)
+#             sys.exit(0)
+#         m += 1
+#     n += 1
