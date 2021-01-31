7ea520f6f6cb15ae1f83e0a293383449f0a66518
Rafel Amer <legorafel@gmail.com>
2021-01-31 19:45:57 +0100

    Working P044

--- P044.py
+++ P044.py
@@ -1,24 +1,54 @@
 import math
 
 
-def IsPentagonal( n ):
+def IsPentagonal(n):
     if n <= 0:
         return False
     n = (1 + math.sqrt(24 * n + 1)) / 6
-    return( (n - int (n)) == 0)
+    return((n - int(n)) == 0)
+#
+# j = 1
+# D = 0
+#
+# while D == 0:
+#     for k in range(1, j):
+#         if IsPentagonal(j) and IsPentagonal(k):
+#             print(j, " ", k)
+#             s = j + k
+#             d = j - k
+#             if IsPentagonal(s) and IsPentagonal(d):
+#                 D = abs(d)
+#     j += 1
+#
+#
+# print(D)
 
-j = 1
 D = 0
+PL = []
+n = 1
+
+
 
 while D == 0:
-    for k in range(1, j):
-        if IsPentagonal(j) and IsPentagonal(k):
-            print(j, " ", k)
-            s = j + k
-            d = j - k
-            if IsPentagonal(s) and IsPentagonal(d):
-                D = abs(d)
-    j += 1
+    PL.append(n*(3*n-1)//2)
+    sum = PL[-1]
+    for j in range(1, len(PL)):
+        for k in range(1, j):
+            PJ, PK = PL[j], PL[k]
+            if PJ + PK == sum:
+                print(PK, " ", PJ, " ", sum)
+                if IsPentagonal(PJ - PK):
+                    D = abs(PJ - PK)
 
 
 print(D)
+
+
+
+
+
+
+
+
+
+    n += 1
