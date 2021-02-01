867c944a664647c946e30d2ca4af77f612011590
Rafel Amer <legorafel@gmail.com>
2021-02-01 17:21:51 +0100

    C Solved P001.c

---
+++ C/P001.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+int main() {
+  int sol = 0;
+  for (int i = 0; i < 1000; i++) {
+    if (i % 3 == 0 || i % 5 ==0) {
+      sol += i;
+    }
+  }
+  printf("%d\n", sol);
+  return 0;
+}
