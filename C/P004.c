4813a7046cee4341da92d8051e7d01d7af5518a1
Rafel Amer <legorafel@gmail.com>
2021-02-27 19:31:55 +0100

    Solved P004.c

--- C/P004.c
+++ C/P004.c
@@ -1,9 +1,27 @@
-//
-// Created by rafel on 14/2/21.
-//
+#include <stdio.h>
+#include <string.h>
 
-int main(int argc, char const *argv[])
-{
-    
+int IsPalindrome(int num){
+    char pal[10];
+    sprintf(pal,"%d", num);
+    int len = strlen(pal);
+    for(int i = 0;i < len/2; i++){
+        if (pal[i] != pal[len-i-1])
+            return 0;   
+    }
+    return 1;
+}
+
+int main(int argc, char const *argv[]){
+    int max = 0;
+    for(int a = 100; a < 1000; a++){
+        for(int b = a; b < 1000; b++){
+            int product = a * b;
+            if(IsPalindrome(product) && (product > max)) 
+                max = product;
+         }
+    }
+    printf("The maximum palindrome is: %d\n", max);
     return 0;
 }
+
