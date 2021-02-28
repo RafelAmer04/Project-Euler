d7072ed01c72513b8e647aa8ea37430e1db27ab7
Rafel Amer <legorafel@gmail.com>
2021-02-28 20:43:23 +0100

    Started P006.c

---
+++ C/P006.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+    int sol = 0, a = 0, b = 0;
+    for(int i = 1;i < 101; i++){
+        a += i**2;
+        b *+= 1;
+    }
+    sol = (b**2) -
+    printf("%b\n", sol)
+    return 0;
+}
