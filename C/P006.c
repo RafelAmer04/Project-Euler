bb7d839f258e8911084955ae5547b501ba7615f9
Rafel Amer <legorafel@gmail.com>
2021-03-02 19:15:19 +0100

    Solved P008

--- C/P006.c
+++ C/P006.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-
-int power(int x, int n){
-    int sol = 1;
-    for (int i = 0; i < n; i++){
-        sol *= x;
-    }
-    return sol;
-}
+#include <math.h>
 
 int main(int argc, char const *argv[]){
     int sol = 0, a = 0, b = 0;
     for(int i = 1;i < 101; i++){
-        a += power(i,2);
+        a += pow(i,2);
         b += i;
     }
-    sol = power(b,2) - a;
+    sol = pow(b,2) - a;
     printf("%d\n", sol);
     return 0;
 }
