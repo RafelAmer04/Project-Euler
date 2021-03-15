73c62d80235649e622d36c31c7be8b522de21ae6
Rafel Amer <legorafel@gmail.com>
2021-03-15 19:55:07 +0100

    Solved P008.c

--- C/P008.c
+++ C/P008.c
@@ -1,17 +1,70 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
+#define ALLOCSIZE 1024
+
+char *digitsFromFile(const char *filename)
+{
+  /*
+    Open the file
+  */
+	FILE *fp;
+	if ((fp = fopen(filename, "r")) == NULL)
+		return NULL;
+  /*
+    Local variables
+  */
+	int c;
+	char *str, *new;
+	size_t alloc_size = ALLOCSIZE, str_size = 0;
+	if ((str = (char *)calloc(alloc_size,sizeof(char))) == NULL)
+		return NULL;
+  /*
+    Read the file
+  */
+  while ((c = getc(fp)) != EOF)
+  {
+    if (!isdigit(c))
+      continue;
+    if (str_size == alloc_size - 1)
+    {
+			alloc_size += ALLOCSIZE;
+			if ((new = (char *)realloc(str,alloc_size * sizeof(char))) == NULL)
+      {
+        free(str);
+				return NULL;
+      }
+      str = new;
+		}
+    str[str_size++] = c;
+  }
+	str[str_size] = '\0';
+  /*
+    Close the file and return the value
+  */
+  fclose(fp);
+	return str;
+}
 
 
 
 int main(int argc, char const *argv[]){
-    char arr[1050] ="7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+    char *data;
+    if((data = digitsFromFile("P008.txt")) == NULL){
+        printf("Error!\n");
+        return 1;
+    }
+    size_t len = strlen(data);
     unsigned long int p = 1;
     unsigned long int sol = 0;
 
-    for (int i = 0; i < 987; i++){
+    for (int i = 0; i < len; i++){
         p = 1;
         for (int j = 0; j < 13; j++){
-            p *= arr[i+j] - '0';
+            p *= data[i+j] - '0';
         }
         if (p > sol){
             sol = p;
