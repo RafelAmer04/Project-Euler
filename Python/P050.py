16eb54ce1ef8b80bf04b9fe638b824a86d5ba360
Rafel Amer <legorafel@gmail.com>
2021-04-17 18:14:59 +0200

    .

--- Python/P050.py
+++ Python/P050.py
@@ -1,10 +1,10 @@
 from functions.functions import *
 
-PL = []
-SPL = [0]
+PL = [2]
+SPL = [0,2]
 
 
-for i in range(1, 1000001):
+for i in range(3, 1000001, 2):
     if IsPrime(i):
         PL.append(i)
         SPL.append(SPL[-1] + i)
@@ -13,9 +13,12 @@ for i in range(1, 1000001):
 l = 0
 sol = 0
 
-for p in range(len(PL)):
-    for n in range(p+1, len(PL)):
-        s = SPL[p] - SPL[n]
+for p in range(len(SPL)):
+    k = SPL[p]
+    for n in range(p+1, len(SPL)):
+        s = k - SPL[n]
+        if s > 1000000:
+            continue
         if s in PL and n-p > l:
             l = n-p
             sol = s
