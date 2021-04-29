9b761b64b12ed51e40bbd5f09cd57fa46c4f5bae
Rafel Amer <legorafel@gmail.com>
2021-04-29 19:45:53 +0200

    Solved P060.py

--- Python/P060.py
+++ Python/P060.py
@@ -3,23 +3,24 @@
 #
 
 import networkx as nx
-from Primes import *
+from functions.functions import IsPrime
 
 primes = [2]
 G = nx.Graph()
 
 for k in range(3,10001):
-    if is_prime(k):
+    if IsPrime(k):
         primes.append(k)
 
 for i in range(len(primes)):
     for j in range(i+1,len(primes)):
         s1 = str(primes[i])
         s2 = str(primes[j])
-        if is_prime(int(s1+s2)) and is_prime(int(s2+s1)):
+        if IsPrime(int(s1+s2)) and IsPrime(int(s2+s1)):
             G.add_edge(primes[i],primes[j])
 
 cliques = nx.find_cliques(G)
 for c in cliques:
     if len(c) >= 5:
+        print(c)
         print(sum(c))
