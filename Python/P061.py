26685f4f9eeaf6dbd41162d78cdfac250092410a
Rafel Amer <legorafel@gmail.com>
2021-07-30 20:11:20 +0200

    Worked on P061.py

--- Python/P061.py
+++ Python/P061.py
@@ -1,5 +1,16 @@
+import sys
+
+
 TRIANGULAR, SQUARE, PENTAGONAL, HEXAGONAL,  HEPTAGONAL, OCTAGONAL = [], [], [], [], [], []
-TRI, SQU, PEN, HEX, HEP, OCT = 0, 0, 0, 0, 0, 0 
+TRI, SQU, PEN, HEX, HEP, OCT = 0, 0, 0, 0, 0, 0
+
+POLIGONAL = {"TRIANGULAR": TRIANGULAR,
+            "SQUARE":SQUARE,
+            "PENTAGONAL": PENTAGONAL,
+            "HEXAGONAL":HEXAGONAL,
+            "HEPTAGONAL": HEPTAGONAL,
+            "OCTAGONAL": OCTAGONAL}
+
 n = 0
 count = 0
  
@@ -55,4 +66,21 @@ while count < 6:
             OCTAGONAL.append(l)
 
 
-print(len(TRIANGULAR),len(SQUARE), len(PENTAGONAL), len(HEXAGONAL), len(HEPTAGONAL),len(OCTAGONAL))
+for i in range(10, 100):
+    for j in range(10, 100):
+        for k in range(10, 100):
+            for l in range(10, 100):
+                for m in range(10, 100):
+                    for n in range(10, 100):
+
+                        CC = {100*i+j,100*j+k,100*k+l,100*l+m,100*m+n, 100*n+i}
+                        if len(CC) != 6:
+                            continue
+                        Q = set()
+                        for x in CC:
+                            for t,s in POLIGONAL.items():
+                                if x in s:
+                                    Q.add(t)
+                        if len(Q) == 6:
+                            print(CC)
+                            sys.exit(0)
