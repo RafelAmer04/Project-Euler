f31fae40701bb87455581fd08cfebceb1bec73fb
Rafel Amer <legorafel@gmail.com>
2021-08-03 19:22:06 +0200

    Solved P065.py

--- Python/P065.py
+++ Python/P065.py
@@ -1,16 +1,26 @@
 from sympy import *
 
 def ContinousFraction(e):
-    p = e[0] + Rational(1, e[1]+Rational(1, e[2] + Rational(1,e[3])))
-    return p
+    f = Rational(1,e[-1])
+    for i in range(len(e)-2, -1, -1):
+        f = e[i] + Rational(1,f)
+    return f
 
     
 e = [2]
+sol = 0
 
-for i in range(1, 2):
+for i in range(1, 105):
     e.append(1)
     e.append(2*i)
     e.append(1)
 
 
-print(ContinousFraction(e))
+f = ContinousFraction(e[0:100])
+
+s = str(f.p)
+
+for j in s:
+    sol += int(j)
+
+print(sol)
