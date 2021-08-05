e22b27a9e9eea46149cf62d4efacfc092a2ae736
Rafel Amer <rafel.amer@upc.edu>
2021-08-05 13:33:48 +0200

    Solved P068.py

--- Python/P068.py
+++ Python/P068.py
@@ -1,28 +1,24 @@
 from itertools import permutations
 
-##N = range(1,11)
-##lines = ((0,5,6),(1,6,5),(2,7,8),(3,8,9),(4,9,5))
-N = range(1,7)
-lines = ((0,3,4),(1,4,5),(2,5,3))
-VALID = []
+N = range(1,11)
+lines = ((0,5,6),(1,6,7),(2,7,8),(3,8,9),(4,9,5))
+sol = 0
 
 for p in permutations(N):
-    t = [[p[l[k]] for k in l] for l in lines]
-    t = set(map(sum(t)))
-
-    if len(t) > 1:
+    t = [[p[k] for k in l] for l in lines]
+    if len(set(map(sum,t))) > 1:
         continue
 
-    n = p[0:3]
+    n = p[0:5]
     mi = n.index(min(n))
     y = []
+    for l in range(mi, mi + 5):
+        y += t[l%5]
+    value = "".join(map(str,y))
+    if len(value) > 16:
+        continue
+    value = int(value)
+    if value > sol:
+        sol = value
 
-    for l in range(mi, mi + 4):
-        y += t[l%3]
-
-print(VALID)
-
-
-
-
-
+print(sol)
