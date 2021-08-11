aca19432c563e584078c8955c92e6854bf841e67
Rafel Amer <amer@portatil.claimcenter.com>
2021-08-11 16:34:39 +0200

    Solved problem P062.py

--- Python/P062.py
+++ Python/P062.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def ArePermutation(n,m):
     ns = list(str(n))
     ms = list(str(m))
@@ -5,54 +7,45 @@ def ArePermutation(n,m):
     ms.sort()
     return ns == ms
 
+done = False
 
 def test_list_of_cubes(permutations,cubes):
-    if len(cubes) == 0:
-        return permutations
-    if len(permutations) + len(cubes) < 3:
-        return []
-    if len(permutations) == 3:
-        return permutations
+    # print(permutations,cubes)
+    global done
+    if len(permutations) + len(cubes) < 5:
+        return
+    if len(permutations) == 5:
+        print (permutations)
+        done = True
+        return
     for i in range(len(cubes) - 1):
+        call = False
         if len(permutations) == 0:
             permutations.append(cubes[i])
+            call = True
         elif ArePermutation(permutations[-1],cubes[i]):
             permutations.append(cubes[i])
-        cubes = cubes[i+1:]
-        r = test_list_of_cubes(permutations, cubes)
-        if len(r) == 3:
-            return r
-        if len(permutations) != 0:
-            permutations.pop()
-    return []
+            call = True
+        if call :
+            test = list(cubes[i+1:])
+            test_list_of_cubes(permutations, test)
+            if len(permutations) != 0:
+                permutations.pop()
+        if done:
+            break
 
 limit = 8
 
-while limit < 9:
-    k1 = int(10**(limit/3))
+while True:
+    m = limit - 1
+    k1 = int(10**(m/3))
 
-    if k1**3 < 10**limit:
+    if k1**3 < 10**m:
         k1 += 1
 
-    k2 = int((10**(limit+1)-1)**(1/3))
+    k2 = int((10**(m+1)-1)**(1/3))
     l = [x**3 for x in range(k1, k2+1)]
-    r = test_list_of_cubes([],l)
-    if len(r) == 3:
-        print (r)
-
-
-
-
+    test_list_of_cubes([],l)
+    if done:
+        break
     limit += 1
-
-
-
-
-
-
-
-
-
-
-
-
