73f9e1c3200a7a436dc7ab99e07082f2a8c28a5e
Rafel Amer <amer@portatil.claimcenter.com>
2021-08-13 13:24:31 +0200

    Solved problem P066.py

--- Python/P066.py
+++ Python/P066.py
@@ -1,27 +1,42 @@
-from math import sqrt
+from sympy import *
 
 def IsSquare(n):
     s = sqrt(n)
     return s == int(s)
 
+def IsPellSolution(D,x,y):
+    return x**2 - D*y**2 == 1
+
+def ContinousFraction(e):
+    f = e[-1]
+    for i in range(len(e)-2, -1, -1):
+        f = e[i] + Rational(1,f)
+    return f
+
+def FindPellSolution(n):
+    REPETITION = []
+    a = int(sqrt(n))
+    b = sqrt(n) - a
+    CONVERGENTS = [a]
+    while True:
+        a = int(1 / b)
+        b = simplify(1 / b - a)
+        f = ContinousFraction(CONVERGENTS)
+        if isinstance(f,Rational) and IsPellSolution(n,f.p,f.q):
+            return f.p,f.q
+        CONVERGENTS.append(a)
+        REPETITION.append(b)
+
 maxX = 0
-sol = 0 
+sol = 0
 
-
-for D in range(1001):
+for D in range(2,1001):
     if IsSquare(D):
         continue
 
-    x = 1
+    x,y = FindPellSolution(D)
+    if x > maxX:
+        maxX = x
+        sol = D
 
-    while True:
-        x += 1
-        t = x**2-1
-        if t % D == 0 and IsSquare(t/D):
-            if x > maxX:
-                maxX = x
-                sol = D
-            break
-
-print(sol)
-
+print(sol, maxX)
--- Python/P073.py
+++ Python/P073.py
@@ -1,12 +1,13 @@
 from math import gcd
 
-
 sol = 0
 
-for d in range(3, 12001):
+for d in range(1, 12001):
     k1 = int(d / 3) + 1
     k2 = int(d / 2)
-    for n in range(k1, k2+1):
+    if 2 * k2 != 2:
+        k2 += 1
+    for n in range(k1, k2):
         if gcd(n, d) == 1:
             sol += 1
 
