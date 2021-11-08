0c5ae8ddba685fdbd90a336b47be2b5d8ee4d5e0
Rafel Amer <legorafel@gmail.com>
2021-11-08 20:26:13 +0100

    Solved p002.rs

--- Rust/src/bin/p002.rs
+++ Rust/src/bin/p002.rs
@@ -1,3 +1,21 @@
 fn main() {
-    
+    println!("{}", compute())
+}
+
+fn compute() -> u32 {
+    let mut solution = 0;
+    let mut x = 1;
+    let mut y = 2;
+    let mut t = 0;
+
+    while x <= 4000000 {
+        t = x+y;
+        x = y;
+        y = t;
+
+        if x % 2 == 0 {
+            solution += x
+        }
+    }
+    return solution;
 }
