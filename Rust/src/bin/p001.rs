1b82d9cfcaeb97db8a6042dc50f5244054b8491d
Rafel Amer <legorafel@gmail.com>
2021-11-08 20:09:21 +0100

    Solved p001.rs

--- Rust/src/bin/p001.rs
+++ Rust/src/bin/p001.rs
@@ -1,3 +1,15 @@
-fn main() {
+// Problem 001 (https://projecteuler.net/problem=1) solver
+fn compute() -> u32 {
+    let mut solution = 0;
 
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            solution += i;
+        }
+    }
+    return solution;
+}
+
+fn main() {
+    println!("{}", compute())
 }
